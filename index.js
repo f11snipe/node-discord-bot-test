25329c66d7378c180b0c74fa304fdd418763c174
Charlie McClung <charlie@bowtie.co>
2024-11-14 13:30:34 -0700

    examples with basic commands

--- index.js
+++ index.js
@@ -1,22 +1,76 @@
 // Initialize dotenv
 require('dotenv').config();
 
-// Discord.js versions ^13.0 require us to explicitly define client intents
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { CLIENT_TOKEN } = process.env;
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        }
+        else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// Log In our bot
-client.login(process.env.CLIENT_TOKEN);
-
-client.on('messageCreate', msg => {
-  console.log(msg);
-  // You can view the msg object here with console.log(msg)
-  if (msg.content === 'Hello') {
-    msg.reply(`Hello ${msg.author.username}`);
-  }
+// TODO: Not working to handle message events (discord.js v14+)?
+client.on(Events.MessageCreate, msg => {
+    console.log(msg.author, msg.content, msg);
+    // You can view the msg object here with console.log(msg)
+    if (msg.content === 'Hello') {
+        msg.reply(`Hello ${msg.author.username}`);
+    }
 });
 
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    }
+    catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+        else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
+});
+
+
+// Log in to Discord with your client's token
+client.login(CLIENT_TOKEN);
